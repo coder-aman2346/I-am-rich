8093e8d21402cfb4887df0084af220e3458307b3
coder <amanprasad1072@gmail.com>
2023-07-07 01:30:00 +0530

    images changed

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,9 @@ void main() {
         title: Text("I am rich"),
       ),
       body: Center(
-        child: FadeInImage(
-          placeholder: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4NmeKROWftG9o8CjYjWjgBJRuT1O6cWnXcw&usqp=CAU'), // Placeholder image while loading
-          image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4NmeKROWftG9o8CjYjWjgBJRuT1O6cWnXcw&usqp=CAU'),
-          fadeInDuration: Duration(milliseconds: 500), // Duration of the fade-in animation
+        child:Image(
+           image: AssetImage('images/Diamond.png'),
+
         ),
       ),
     ),
